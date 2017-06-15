4e896054b5bbfe49cee72a89367f29863abc6d19
Ariel Blasi <exorcismo@gmail.com>
2017-06-15 19:50:03 -0300

    add eslint and hookah

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.post("/blogs",(req,res) => {
 	//takes the data (req.body.blog) and creates a new blog
 	//.blog contains 3 groupedd items
 	//after creation, it executes the callback, which is go back to blogs list
-	Blog.create(req.body.blog, (err, newBlog) => {
+	//note: originally was (err, newBlog) but ESLint complained that newBlog was defined but not used, so:
+	Blog.create(req.body.blog, (err) => {
 		//if error, just show the form again
 		if(err){
 			res.render("new")
@@ -66,24 +67,25 @@ app.get("/blogs/:id",(req,res) => {
 	})
 })
 
-
-
 //check connection
 var db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", function() {
-  // we're connected!
+// we're connected!
 	console.log("WE ARE CONNECTED REALLY")
 })
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku, on localhost or codeanywhere 3000
+var port = process.env.PORT || 3000
+
 //check connection through express
-//with codeanywhere port 3000 works, on heroku it's process.env.PORT
-app.listen(process.env.PORT, function () {
-  console.log('YelpCamp app listening on port 3000!')
+app.listen(port, function() {
+	console.log("Our app is running on http://localhost:" + port)
 })
 
 //Asterisk for a 404 route
 app.get("*", function(req, res){
-    res.send("404 NOTHING TO SEE HERE...")
+	res.send("404 NOTHING TO SEE HERE...")
 })
 
